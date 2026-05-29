e08745be239a6f3730c8ef80e4f091ebeab157a3
Mahmoud767 <mgalfaki@gmail.com>
2026-05-29 22:35:06 +0400

    feat: remove Libra AI, Pricing, Documentation from nav and remove GitHub star icon

--- apps/nextjs/src/components/navbar.tsx
+++ apps/nextjs/src/components/navbar.tsx
@@ -10,7 +10,6 @@ import { Button } from "@saasfly/ui/button";
 
 import { MainNav } from "./main-nav";
 import { LocaleChange } from "~/components/locale-change";
-import { GitHubStar } from "~/components/github-star";
 import { useSigninModal } from "~/hooks/use-signin-modal";
 import { UserAccountNav } from "./user-account-nav";
 
@@ -80,9 +79,6 @@ export function NavBar({
 
           {rightElements}
 
-          <div className="hidden md:flex lg:flex xl:flex">
-            <GitHubStar />
-          </div>
           <LocaleChange url={"/"} />
           {!user ? (
             <Link href={`/${lang}/login-clerk`}>
